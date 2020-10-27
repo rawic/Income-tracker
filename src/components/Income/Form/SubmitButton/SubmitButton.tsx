d23c28b540e0e8f06d8a416063eecb294b459f38
rawic <hi@rawic.me>
2020-10-27 20:18:46 +0100

    Rewrite SubmitButton component to TypeScript

--- src/components/Income/Form/SubmitButton/SubmitButton.tsx
+++ src/components/Income/Form/SubmitButton/SubmitButton.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import StyledSubmitButton from './SubmitButton.styles';
 
-const SubmitButton = ({ children }) => (
+const SubmitButton: React.FC<{ children: string}> = ({ children }) => (
   <StyledSubmitButton type="submit">{children}</StyledSubmitButton>
 );
-
-SubmitButton.propTypes = {
-  children: PropTypes.node.isRequired,
-};
 
 export default SubmitButton;
